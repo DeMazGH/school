e4ad834ebfc77eb39790136c239d3966545bc28b
Denis <demaz@bk.ru>
2022-12-11 13:40:16 +0700

    SQL query

---
+++ src/script.sql
@@ -0,0 +1,6 @@
+select * from student;
+select * from student where age > 10 and age < 17;
+select name from student;
+select * from student where name like '%а%';
+select * from student where age < student.id;
+select * from student order by age;
